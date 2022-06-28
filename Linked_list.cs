107239de9fa52533d1298b826528d1ec112231f9
unknown <abhisheksrivastava9719@gmail.com>
2022-06-28 23:49:34 +0530

    Welcome to Linked List

--- Linked_list.cs
+++ Linked_list.cs
@@ -7,43 +7,8 @@ namespace LinkedList
 {
        public class linked_list
         {
-<<<<<<< HEAD
-            Node node = new Node(a);
-            this.head = node;
-            Console.WriteLine("Node created with "+head.data);
-        }
-        public void add(int a)
-        {
-             Node node = new Node(a);
-             Node temp = head;
-             while (temp.next != null)
-             {
-                 temp = temp.next;
-             }
-            temp.next = node;
-            Console.WriteLine("Node added with data " + temp.next.data);
-
-        }
-        public void append(int a)
-        {
-            Node node = new Node(a);
-            Node temp = head;
-            while (temp.next != null)
-            {
-                temp = temp.next;
-            }
-            temp.next = node;
-            Console.WriteLine("Node appended with data " + temp.next.data);
-
-        }
-        public void print()
-        {
-            Node temp = this.head;
-            while (temp != null)
-=======
             internal Node head;
             public void create(int a)
->>>>>>> addToList
             {
                 Node node = new Node(a);
                 this.head = node;
