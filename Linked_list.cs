f6edf65f0415b763e66116b6322ff2e492819f7d
unknown <abhisheksrivastava9719@gmail.com>
2022-06-28 23:41:01 +0530

    Create and add numbers

--- Linked_list.cs
+++ Linked_list.cs
@@ -5,35 +5,32 @@ using System.Text;
 using System.Threading.Tasks;
 namespace LinkedList
 {
-    public class linked_list
-    {
-        internal Node head;
-        public void create(int a)
+       public class linked_list
         {
-            Node node = new Node(a);
-            this.head = node;
-            Console.WriteLine("Node created with "+head.data);
-        }
-        public void add(int a)
-        {
-             Node node = new Node(a);
-             Node temp = head;
-             while (temp.next != null)
-             {
-                 temp = temp.next;
-             }
-            temp.next = node;
-            Console.WriteLine("Node added with data " + temp.next.data);
-
-        }
-        public void print()
-        {
-            Node temp = this.head;
-            while (temp != null)
+            internal Node head;
+            public void create(int a)
             {
-                Console.WriteLine(temp.data + " ");
-                temp = temp.next;
+                Node node = new Node(a);
+                this.head = node;
+                Console.WriteLine("Node created with " + head.data);
+            }
+            public void add(int b)
+            {
+                Node node = new Node(b);
+                Node temp = head;
+                temp.next = head.next;
+                head = node;
+                head.next = temp;
+                Console.WriteLine("Node added with data " + head.data);
+            }
+            public void print()
+            {
+                Node temp = this.head;
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data + " ");
+                    temp = temp.next;
+                }
             }
         }
-    }
 }
