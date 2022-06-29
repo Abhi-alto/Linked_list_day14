73c021ff963c9bcabd644dc095a0ee099bc1ddbc
unknown <abhisheksrivastava9719@gmail.com>
2022-06-29 08:13:05 +0530

    Created added and then inserted a node

--- Linked_list.cs
+++ Linked_list.cs
@@ -5,26 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 namespace LinkedList
 {
-       public class linked_list
+    public class linked_list
+    {
+        internal Node head;
+        public void create(int a)               //creating a node with data
         {
-<<<<<<< HEAD
             Node node = new Node(a);
             this.head = node;
-            Console.WriteLine("Node created with "+head.data);
+            Console.WriteLine("Node created with " + head.data);
         }
-        public void add(int a)
+        public void add(int b)                  //adding a node in the begining
         {
-             Node node = new Node(a);
-             Node temp = head;
-             while (temp.next != null)
-             {
-                 temp = temp.next;
-             }
-            temp.next = node;
-            Console.WriteLine("Node added with data " + temp.next.data);
-
+            Node node = new Node(b);
+            Node temp = head;
+            temp.next = head.next;
+            head = node;
+            head.next = temp;
+            Console.WriteLine("Node added with data " + head.data);
         }
-        public void append(int a)
+        public void append(int a)           //adding nodes to the end
         {
             Node node = new Node(a);
             Node temp = head;
@@ -34,38 +33,44 @@ namespace LinkedList
             }
             temp.next = node;
             Console.WriteLine("Node appended with data " + temp.next.data);
-
         }
-        public void print()
+        public void insert(int inser,int position)              //adding nodes to a particular position
+        {
+            Node node = new Node(inser);
+            Node temp = head;
+            int posi = 1;
+            if (position < 1)
+            {
+                Console.WriteLine("Wrong position");
+            }
+            else if(position==1)
+            {
+                Node newNode = node;
+                newNode.next = temp;
+                head = newNode;
+                Console.WriteLine("Inserted new node with data " + newNode.data);
+            }
+            else
+            {
+                while (posi != position - 1)
+                {
+                    temp = temp.next;
+                    posi++;
+                }
+                Node newNode = node;
+                newNode.next = temp.next;
+                temp.next=newNode;
+                Console.WriteLine("Inserted new node with data "+newNode.data);
+            }
+        }
+         public void print()                //printing the nodes
         {
             Node temp = this.head;
             while (temp != null)
-=======
-            internal Node head;
-            public void create(int a)
->>>>>>> addToList
             {
-                Node node = new Node(a);
-                this.head = node;
-                Console.WriteLine("Node created with " + head.data);
-            }
-            public void add(int b)
-            {
-                Node node = new Node(b);
-                Node temp = head;
-                temp.next = head.next;
-                head = node;
-                head.next = temp;
-                Console.WriteLine("Node added with data " + head.data);
-            }
-            public void print()
-            {
-                Node temp = this.head;
-                while (temp != null)
-                {
-                    Console.WriteLine(temp.data + " ");
-                    temp = temp.next;
-                }
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
             }
         }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@
 Console.WriteLine("Welcome to the linked list");
 linked_list List = new linked_list();
 List.create(56);
-List.append(30);
-List.append(70);
+List.add(30);
+List.insert(70,1);
 List.print();
